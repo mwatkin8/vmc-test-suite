914017d3505402ccde9763d4e0dd52b010df0c60
mwatkins8 <michael.watkins.8@gmail.com>
2017-09-29 13:36:48 -0600

    version 1 dones, going to start trying other formats

---
+++ UI/js/endpoint.js
@@ -0,0 +1,75 @@
+//
+// Helper function allows use of "$()" instead of "document.getElementById()"
+//
+function $(id) { return document.getElementById(id) }
+
+//
+// When the page loads:
+// 1. Set up external links as pop-ups
+// 2. Replace unimplemented links with a message
+// 3. Add onclick events to the navigation menu so when items are clicked they
+//    are highlighted
+//
+window.onload = function() {
+	// Loop through links
+	links = document.getElementsByTagName('a')
+	for(i=0; i<links.length; i++ ) {
+		link = links[i]
+
+		// Set up links with class="exLink" as full screen pop ups
+		if(link.className && link.className.indexOf('exLink')>-1) {
+			link.href = "javascript:void window.open('" + link.href
+			link.href += "','','width='+screen.width+',height='+screen.height+',resizable=1,directories=1,menubar=1,toolbar=1,scrollbars=1').focus()"
+		}
+
+		// Set up links with class="popUp" as centered pop ups without toolbars
+		if(link.className && link.className.indexOf('popUp')>-1) {
+			x = Math.floor(screen.width/2) - 375
+			y = Math.floor(screen.height/2) - 325
+			link.href = "javascript:void window.open('" + link.href
+			link.href += "','EMR','width=750,height=550,top='+y+',left='+x+',toolbar=0,scrollbars=1').focus()"
+		}
+
+
+		// All "Top" links in the document should select the Top (first) item in the nav menu
+		if(endsWith(link.href,"#top")) {
+			// Simulate a click on the "Top" nav item (which has an id of navTop)
+			link.onclick = function() { selectFragment($("navTop")) }
+		}
+	}
+
+	// Set up onclick events for links to fragments from the nav menu
+	if($("nav")) {
+		for(i=0; i<$("nav").childNodes.length; i++)
+		$("nav").childNodes[i].childNodes[0].onclick = function() { selectFragment(this) }
+	}
+}
+
+//
+// Returns true if the string x ends with the string y
+//
+function endsWith(x, y) {
+	if(x.length > y.length && x.substr(x.length-y.length,y.length) == y)
+	return true
+	else
+	return false
+}
+
+//
+// This event handler is called when a nav list item is clicked; it selects the
+// appropriate menu item and unselects others
+//
+function selectFragment(el) {
+	// Unselect all the items in the list
+	for(i=0; i<$("nav").childNodes.length; i++)
+	$("nav").childNodes[i].childNodes[0].className = ""
+
+	// Select the clicked list item
+	el.className = "selected"
+}
+
+function ngSwitch(dest){
+  if(dest == 'test'){
+    $(pageVar) = 'grid'
+  }
+}
--- fetchAnnotations.py
+++ fetchAnnotations.py
@@ -1,48 +1,66 @@
 #!/usr/bin/python
-import sys,argparse, requests, json, ast
+import sys,argparse, requests, json, ast, sqlite3, re
+
+
+def pull_variant():
+    id_list = []
+    with sqlite3.connect("../../data_dump/variants.db") as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT ID FROM Variant")
+        rows = cursor.fetchall()
+        for row in rows:
+            val = row[0]
+            id_list.append(str(val))
+    print(id_list)
 
 #Get the variant based on the identifier (digest)
 def fetch_variant(identifier):
-	
 	return identifier
 
 #Connect to the VEP annotation tool
 def VEP(variant):
 	server = "https://rest.ensembl.org"
 	ext = "/vep/human/hgvs/" + variant
-	
 	r = requests.get(server+ext, headers = {"Content-Type" : "application/json"})
-
 	if not r.ok:
 		r.raise_for_status()
 		sys.exit()
-
 	decoded = r.json()
 	decoded = ast.literal_eval(repr(decoded))
 	return json.dumps(decoded)
-	
+
 #Parse output from VEP json
 def parse_VEP_json(vep_json):
 	with open("output", "w") as out:
-		j = json.loads(vep_json)		
+		j = json.loads(vep_json)
 		print "json loaded"
-
 		for field in j:
 			if field["transcript_consequences"]:
 				for consequence in field["transcript_consequences"]:
 					out.write(
 						"Gene_symbol: " + consequence["gene_symbol"] +
-						"\nBiotype: " + consequence["biotype"] + 
-						"\nConsequence_terms: " + consequence["consequence_terms"][0] + 
-						"\nImpact: " + consequence["impact"] + "\n\n") 
+						"\nBiotype: " + consequence["biotype"] +
+						"\nConsequence_terms: " + consequence["consequence_terms"][0] +
+						"\nImpact: " + consequence["impact"] + "\n\n")
 
-def main(args):
-	sys.stderr.write("Gathering annotations for: " + args.identifier + "\n")
-	variant = fetch_variant(args.identifier)
+def main():
+    #1 Parse the VCF
+    #2 (once we have it up and running) query the database for that variant
+    #3 If not there:
+        #Digest it
+        #Fetch variants for it
+        #Add it to database
+    #4 If it is there:
+        #Ask which tools to see annotations
+        #Ask which annotation fields to filter
 
-	parse_VEP_json(VEP(variant))	
+	pull_variant()
+
+#	parse_VEP_json(VEP(variant))
 
 if __name__ == "__main__":
+    main()
+    """
 	parser = argparse.ArgumentParser(
 		description = "Gathers annotation information for a VMC standard variant")
 	parser.add_argument( 'identifier',
@@ -50,5 +68,4 @@ if __name__ == "__main__":
 		help = "Identifier should be the digest of a VMC standard variant"
 		)
 	args = parser.parse_args()
-
-	main(args)	
+    """
