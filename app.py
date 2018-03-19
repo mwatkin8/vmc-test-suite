124b38ca183ba667c7d67b84a6561a998694e3e6
mwatkin8 <michael.watkins@utah.edu>
2018-03-19 09:35:20 -0600

    modified variable names for clarity

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, render_template, \
     request, redirect, send_from_directory
-import transform, bundle, conversions, re, os
+import vcf_transform, json_bundle, hgvs_conversion, re, os
 
 # create the application object
 APP = Flask(__name__, static_folder='static')
@@ -12,14 +12,14 @@ def get_upload(filename):
     with open("static/uploads/" + filename) as f_in:
         return f_in.read()
 
-def get_transform(filename):
+def get_vmc_vcf(filename):
     """Returns the transformed VCF file as a string"""
     with open(filename) as f_out:
         return f_out.read()
 
-def get_json_example():
+def get_json_schema():
     """Returns the example JSON schema file as a string"""
-    with open("static/example.json") as f_in:
+    with open("static/schema.json") as f_in:
         return f_in.read()
 
 def get_json_bundle(filename):
@@ -32,7 +32,7 @@ def get_filename():
     r = re.compile('.+.vcf')
     return list(filter(r.match, os.listdir("static/uploads")))[0]
 
-def get_out_paths():
+def get_download_paths():
     """Creates paths to transformed VCF and JSON files ready for download"""
     r = re.compile('.+.vcf')
     filename = list(filter(r.match, os.listdir("static/uploads")))[0]
@@ -47,60 +47,59 @@ def home():
         Displays the example JSON schema, saves the uploaded VCF file to the uploads folder and then displays it as well.
 
     """
-    json_example = get_json_example()
+    json_schema = get_json_schema()
     if request.method == 'POST':
         #check if the post request has the file part
         if 'file' not in request.files:
             return redirect(request.url)
         file = request.files['file']
         file.save(os.path.join(APP.config['UPLOAD_FOLDER'], file.filename))
-        vcf_in = get_upload(file.filename)
+        vcf_upload = get_upload(file.filename)
         #Displays uploaded VCF with example JSON
-        return render_template('index.html',vcf_in=vcf_in,json_example=json_example)
+        return render_template('index.html',vcf_upload=vcf_upload,json_schema=json_schema)
     #Displays example JSON
-    return render_template('index.html',json_example=json_example)
+    return render_template('index.html',json_schema=json_schema)
 
 
-@APP.route('/transformed_vcf', methods=['GET', 'POST'])
-def display_transformed():
+@APP.route('/vmc_vcf', methods=['GET', 'POST'])
+def display_vmc_vcf():
     """
         Checks to see if the transformed VCF already exists in the downloads folder, generates it if not from transform.py, then displays it along with the
         example JSON schema and the original uploaded file. Also sends the filepath for downloading the transformed VCF file.
     """
-    json_example = get_json_example()
+    json_schema = get_json_schema()
     filename = get_filename()
-    json,vcf = get_out_paths()
-    vcf_in = get_upload(filename)
+    json_path,vmc_vcf_path = get_download_paths()
+    vcf_upload = get_upload(filename)
     #Check if transformed VCF exists in downloads folder
     r = re.compile('.+.vcf')
     if filter(r.match, os.listdir("static/downloads")):
-        transform.run(filename, vcf)
-        transformed_vcf = get_transform(vcf)
+        vcf_transform.run(filename, vmc_vcf_path)
+        vmc_vcf = get_vmc_vcf(vmc_vcf_path)
     else:
-        transformed_vcf = get_transform(vcf)
-    return render_template('index.html', json_example=json_example, vcf_in=vcf_in, transformed_vcf=transformed_vcf, vcf_outPath=vcf)
+        vmc_vcf = get_vmc_vcf(vmc_vcf_path)
+    return render_template('index.html', json_schema=json_schema, vcf_upload=vcf_upload, vmc_vcf=vmc_vcf, vmc_vcf_path=vmc_vcf_path)
 
 
-@APP.route('/bundle', methods=['GET', 'POST'])
-def display_bundle():
+@APP.route('/json_bundle', methods=['GET', 'POST'])
+def display_json_bundle():
     """
         Checks to see if the transformed JSON already exists in the downloads folder, generates it if not from bundle.py, then displays it along with the
         example JSON schema and the original uploaded file. Also sends the filepath for downloading the transformed JSON file.
 
     """
-    json_example = get_json_example()
+    json_schema = get_json_schema()
     filename = get_filename()
-    json,vcf = get_out_paths()
-    vcf_in = get_upload(filename)
-    json_example = get_json_example()
+    json_path,vmc_vcf_path = get_download_paths()
+    vcf_upload = get_upload(filename)
     #Check if transformed JSON exists in downloads folder
     r = re.compile('.+.json')
     if filter(r.match, os.listdir("static/downloads")):
-        json_bundle = get_json_bundle(json)
+        json_bundle = get_json_bundle(json_path)
     else:
-        bundle.run(filename, json)
-        json_bundle = get_json_bundle(json)
-    return render_template('index.html', json_example=json_example, vcf_in=vcf_in, json_bundle=json_bundle, json_outPath=json)
+        json_bundle.run(filename, json_path)
+        json_bundle = get_json_bundle(json_path)
+    return render_template('index.html', json_schema=json_schema, vcf_upload=vcf_upload, json_bundle=json_bundle, json_path=json_path)
 
 
 @APP.route('/hgvs', methods=['GET', 'POST'])
@@ -109,10 +108,10 @@ def hgvs_to_json():
         Uses conversions.py to convert a HGVS string to a VMC JSON bundle and displays it along with the example JSON schema.
 
     """
-    json_example = get_json_example()
+    json_schema = get_json_schema()
     hgvs = request.form['hgvs_string']
-    hgvs_json = conversions.from_hgvs(hgvs)
-    return render_template('index.html', hgvs_json=hgvs_json, json_example=json_example)
+    hgvs_json = hgvs_conversion.from_hgvs(hgvs)
+    return render_template('index.html', json_schema=json_schema, hgvs_json=hgvs_json)
 
 
 # start the server with the 'run()' method
